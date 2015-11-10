649db942e000c2f6581b0ececbacc2b2b737ebb2
XuanyiZhu <xzhu2@uvm.edu>
2015-11-10 17:47:18 -0500

    Update nav.php

--- nav.php
+++ nav.php
@@ -4,7 +4,7 @@
         <li><figure>
                     <img class ="navLogo" src = "img/logo.png" alt = "uvm theatre department logo" />
             </figure></li>
-        <li class = 'nav'><a href="index.php/#PopularShow">Popular Show</a></li>
-        <li class = 'nav'><a href="index.php/#ComingSoon">Coming Soon</a></li>
+            <li class = 'nav'><a href="index.php#PopularShow">Popular Show</a></li>
+            <li class = 'nav'><a href="index.php#ComingSoon">Coming Soon</a></li>
     </ol>
 </nav>
