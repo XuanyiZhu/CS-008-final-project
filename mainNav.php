ee86aee7a07184d5c4f8f7a69f4fad859b384437
XuanyiZhu <xzhu2@uvm.edu>
2015-11-11 17:22:17 -0500

    Create mainNav.php

---
+++ mainNav.php
@@ -0,0 +1,17 @@
+<!-- ######################     Main Navigation   ########################## -->
+<nav id = "MainNav">
+    <ol>
+        <li class = 'MainNav'><a href="http://www.cartserver.com/sc/cart.cgi?item=b-8021&amp;view.x=1" target="_parent">Cart</a></li>
+
+        <li>
+            
+                <img class = "cart" src = "img/cart.png" alt = "cart" />
+            
+        </li>
+        <li><div class="fb-share-button" data-href="https://www.facebook.com/UVM-Department-of-Theatre-117967131563406/" data-layout="button_count"></div></li>
+        <li><a href="https://twitter.com/share" class="twitter-share-button" data-url=
+               "https://twitter.com/uvmvermont/status/502189309567725568"
+               data-text="Welcome to UVM Theatre" data-hashtags="UVMTheatre">Tweet</a>
+        </li>
+    </ol>
+</nav>
