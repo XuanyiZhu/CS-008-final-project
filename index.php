c1b10e27624643d78bb6fe3c094576ba7334d3ab
XuanyiZhu <xzhu2@uvm.edu>
2015-11-20 15:44:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,34 @@
 <?php
 include'top.php';
+include 'mainNav.php';
 ?>
-/* 
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
-*/
+<h1>UVM Department of Theatre</h1>
 
+<div class="flexslider">
+    <ul class="slides">
+        <?php
+        
+        foreach ($records as $oneRecord) {
+            if ($oneRecord[1] == 'home_slide_pic') {
+
+                for ($i = 2; $oneRecord[$i] != ""; $i++) {
+                    //if ($oneRecord[$i] != "") {
+                        print'<li>';
+                        print'<img src="img/' . $oneRecord[$i] . '" alt="img_slide" >';
+                        print'</li>';
+                    //}
+                }
+            }
+        }
+        ?>
+    </ul>
+</div>
 
 <h2 id ="PopularShow">Popular Show</h2>
 <h2 id = "ComingSoon">Coming Soon</h2>
-        </body>
-        </html>
+
+</body>
+<?php
+include 'footer.php';
+?>
+</html>
