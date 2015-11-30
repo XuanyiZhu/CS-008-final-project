3054d12c755488e50389acbd0c798a619dc5e3fd
XuanyiZhu <xzhu2@uvm.edu>
2015-11-30 18:02:20 -0500

    Update rank.php

--- rank.php
+++ rank.php
@@ -1,34 +1,28 @@
 <?php
-
-
 $Star1 = "star.png";
 $Star2 = "star.png";
 $Star3 = "star.png";
 $Star4 = "star.png";
 $Star5 = "star.png";
-
-
 $fileExt = ".csv";
 $myFileName = "data/rank";
 $filename = $myFileName . $fileExt;
 $file = fopen($filename, 'a+');
 while (!feof($file)) {
-            $rankRecords[] = fgetcsv($file);
-        }
-        
+    $rankRecords[] = fgetcsv($file);
+}
+
 if (isset($_POST["btnRank1"])) {
     $dataRecord[] = $showId;
     $dataRecord[] = 1;
     $Star1 = "yelloStar.jpg";
 }
-
 if (isset($_POST["btnRank2"])) {
     $dataRecord[] = $showId;
     $dataRecord[] = 2;
     $Star1 = "yelloStar.jpg";
     $Star2 = "yelloStar.jpg";
 }
-
 if (isset($_POST["btnRank3"])) {
     $dataRecord[] = $showId;
     $dataRecord[] = 3;
@@ -36,7 +30,6 @@ if (isset($_POST["btnRank3"])) {
     $Star2 = "yelloStar.jpg";
     $Star3 = "yelloStar.jpg";
 }
-
 if (isset($_POST["btnRank4"])) {
     $dataRecord[] = $showId;
     $dataRecord[] = 4;
@@ -45,7 +38,6 @@ if (isset($_POST["btnRank4"])) {
     $Star3 = "yelloStar.jpg";
     $Star4 = "yelloStar.jpg";
 }
-
 if (isset($_POST["btnRank5"])) {
     $dataRecord[] = $showId;
     $dataRecord[] = 5;
@@ -56,46 +48,44 @@ if (isset($_POST["btnRank5"])) {
     $Star5 = "yelloStar.jpg";
 }
 
-foreach ($rankRecords as $rankRecord){
-    $j = 0;
-    $sum = 0;
-    
-    if ($rankRecord[0] == $showId){
-        $j ++;
-        
-        $a = intval("$rankRecord[1]");
-        $b = intval("$sum");
-        $b = $b + $a;
-        print"$b       ";
-        print"hello: $a";
+$j = 0;
+$sum = 0;
+foreach ($rankRecords as $rankRecord) {
+
+
+    if ($rankRecord[0] == $showId) {
+        $j = $j + 1;
+
+        $sum = (int) $sum + (int) $rankRecord[1];
     }
-    $avg = 2.4;
+
+    $avg = $sum / $j;
 }
-if($avg>= 0.5 and $avg<1.5){
+if ($avg >= 0.5 and $avg < 1.5) {
     $Star1 = "redStar.png";
     $Star2 = "star.png";
     $Star3 = "star.png";
     $Star4 = "star.png";
     $Star5 = "star.png";
-}elseif ($avg>= 1.5 and $avg<2.5) {
+} elseif ($avg >= 1.5 and $avg < 2.5) {
     $Star1 = "redStar.png";
     $Star2 = "redStar.png";
     $Star3 = "star.png";
     $Star4 = "star.png";
     $Star5 = "star.png";
-}elseif ($avg>= 2.5 and $avg<3.5) {
+} elseif ($avg >= 2.5 and $avg < 3.5) {
     $Star1 = "redStar.png";
     $Star2 = "redStar.png";
     $Star3 = "redStar.png";
     $Star4 = "star.png";
     $Star5 = "star.png";
-}elseif ($avg>= 3.5 and $avg<4.5) {
+} elseif ($avg >= 3.5 and $avg < 4.5) {
     $Star1 = "redStar.png";
     $Star2 = "redStar.png";
     $Star3 = "redStar.png";
     $Star4 = "redStar.png";
     $Star5 = "star.png";
-}elseif ($avg>= 4.5) {
+} elseif ($avg >= 4.5) {
     $Star1 = "redStar.png";
     $Star2 = "redStar.png";
     $Star3 = "redStar.png";
@@ -103,21 +93,19 @@ if($avg>= 0.5 and $avg<1.5){
     $Star5 = "redStar.png";
 }
 
-
-    
 fputcsv($file, $dataRecord);
 fclose($file);
 ?>
 
 <form method = "post"
-    id = "frmRank">
+      id = "frmRank">
     <label>rank the show: </label>
-    
+
     <input type="image" src="img/<?php print"$Star1" ?>" id = "btnRank1" name =" btnRank1" value ="1" width="20" height="20" />
     <input type="image" src="img/<?php print"$Star2" ?>" id = "btnRank2" name =" btnRank2" value ="2" width="20" height="20" />
     <input type="image" src="img/<?php print"$Star3" ?>" id = "btnRank3" name =" btnRank3" value ="3" width="20" height="20" />
     <input type="image" src="img/<?php print"$Star4" ?>" id = "btnRank4" name =" btnRank4" value ="4" width="20" height="20" />
     <input type="image" src="img/<?php print"$Star5" ?>" id = "btnRank5" name =" btnRank5" value ="5" width="20" height="20" />
-
-
+    <label><?php print "(" . $j; ?> ranks)</label>
 </form>
+
